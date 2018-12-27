73a6408101478d8d389044ef19a2b7815765da9a
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2018-12-28 00:22:08 +0100

    feat(background): implement picture fectching every day

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,5 @@
+BM_PICTURE_FETCH_HOUR = '11'
+
 /**
  * @see https://stackoverflow.com/a/20285053
  * @param {*} url
@@ -17,30 +19,61 @@ function toDataURL(url, callback) {
   xhr.send();
 }
 
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('onInstalled')
-
-  // set default options
-  chrome.storage.local.set({
-    showFeed: false
-  })
-
-  // fetch Bonjour Madame picture
+/**
+ *
+ */
+function fetchPicture() {
   toDataURL('http://giskard.aqelia.com:8888', dataUrl => {
     // store base64 image
     chrome.storage.local.set({
       imageUrl: dataUrl
     })
   })
+}
 
-  // loads quotes
+/**
+ *
+ */
+function fetchQuotes() {
   const quotesUrl = chrome.runtime.getURL('quotes.json')
-  fetch(quotesUrl)
+
+  return fetch(quotesUrl)
     .then(response => response.json())
     .then(quotes => {
       // store quotes in local storage
       chrome.storage.local.set({
         quotes: quotes
       })
-    });
+    })
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+  // set default options
+  chrome.storage.local.set({
+    showFeed: false
+  })
+
+  // fetch Bonjour Madame picture
+  fetchPicture()
+
+  // loads Gainsbourg quotes
+  fetchQuotes()
 });
+
+/**
+ * Set daily picture fecth
+ */
+let todayFetch = new Date()
+todayFetch.setHours(BM_PICTURE_FETCH_HOUR)
+todayFetch.setMinutes(0)
+
+chrome.alarms.create('fetchMadame', {
+  when: todayFetch.getTime(),
+  periodInMinutes: 24*60 // every day
+})
+
+chrome.alarms.onAlarm.addListener(({ name }) => {
+  if (name === 'fetchMadame') {
+    fetchPicture();
+  }
+})
